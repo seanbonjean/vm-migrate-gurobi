ca831fcc74d10398be336f8c9d9d784661b7815f
Sean Bonjean <sean-bonjean@outlook.com>
2024-10-03 19:18:42 +0800

    store before trying another method

--- model/input.py
+++ model/input.py
@@ -15,13 +15,56 @@ class Data:
         return line
 
     def loadData(self):
-        self.vm_count = int(self.nextLine())  # VM的数量
-        self.band_unitcost = float(self.nextLine())  # 带宽单位成本(η)
+        self.vm_count = int(self.nextLine())  # VM的数量(0~n-1)
+        self.pm_count = int(self.nextLine())  # PM的数量(1~n)
+        self.cluster_count = int(self.nextLine())  # 集群的数量(1~n)
+        self.band_unitcost = float(self.nextLine())  # 带宽单位成本(符号:η)
+        self.elasticity_lb = float(self.nextLine())  # 负载弹性下限(符号:τ)
 
-        self.vm_bandwidth = []  # VM需求的带宽
+        self.vm_mycluster = []  # VM所属的集群
         for _ in range(self.vm_count):
-            self.vm_bandwidth.append(int(self.nextLine()))
+            self.vm_mycluster.append(int(self.nextLine()))
 
-        self.vm_storage = []  # VM需求的存储
+        self.vm_mypm = []  # VM所属的物理机
         for _ in range(self.vm_count):
-            self.vm_storage.append(int(self.nextLine()))
+            self.vm_mypm.append(int(self.nextLine()))
+        
+        self.pm_CPUcore = []  # 物理机CPU核心数o(mi)
+        for _ in range(self.pm_count):
+            self.pm_CPUcore.append(int(self.nextLine()))
+
+        self.pm_storage = []  # 物理机存储容量r(mi)
+        for _ in range(self.pm_count):
+            self.pm_storage.append(float(self.nextLine()))
+
+        # 注意此处的带宽指公网带宽，仅作为一项资源，集群内vm间的通信已由“数据量×跳数×单位开销”表示
+        self.pm_bw = []  # 物理机带宽b(mi)
+        for _ in range(self.pm_count):
+            self.pm_bw.append(float(self.nextLine()))
+
+        self.vm_CPUuti = []  # VM需求的CPU利用率c(vk)
+        for _ in range(self.vm_count):
+            self.vm_CPUuti.append(float(self.nextLine()) / 100)
+
+        self.vm_CPUcore = []  # VM需求的CPU核心o(vk)
+        for _ in range(self.vm_count):
+            self.vm_CPUcore.append(int(self.nextLine()))
+
+        self.vm_storage = []  # VM需求的存储r(vk)
+        for _ in range(self.vm_count):
+            self.vm_storage.append(float(self.nextLine()))
+
+        # 注意事项同上
+        self.vm_bw = []  # VM需求的带宽b(vk)
+        for _ in range(self.vm_count):
+            self.vm_bw.append(float(self.nextLine()))
+
+        self.hop_matrix = {}  # 跳数矩阵
+        for i in range(self.pm_count):
+            line = self.nextLine()
+            for j, value in enumerate(line.split()):
+                self.hop_matrix[(i, j)] = int(value)
+
+        self.cluster_innerpakage_size = []  # 集群内数据包大小
+        for _ in range(self.cluster_count):
+            self.cluster_innerpakage_size.append(float(self.nextLine()))
--- model/solver.py
+++ model/solver.py
@@ -8,26 +8,106 @@ if len(sys.argv) != 2:
     print("example of use: ./solver.py input.txt")
     exit(1)
 
+'''
+def find_lesshop(vm_num):
+    cluster_num = d.vm_mycluster[vm_num]
+    same_cluster = []
+    # 寻找同集群的vm
+    for i in range(d.vm_count):
+        if d.vm_mycluster[i] == cluster_num and i != vm_num:
+            same_cluster.append(i)
+    # 如果同集群为空（无同集群vm），则返回0（说明无需vm间通信）
+    if same_cluster == []:
+        return 0
+    # 寻找同集群vm中跳数最少的vm
+    lesshop = math.inf
+    for i in same_cluster:
+        hop = d.hop_matrix[(d.vm_mypm[vm_num], d.vm_mypm[i])]
+        if hop < lesshop:
+            lesshop = hop
+            target_vm = i
+            target_pm = d.vm_mypm[i]
+    return (lesshop, target_vm, target_pm)
+'''
+
+
+def cal_hops(vm_num):
+    cluster_num = d.vm_mycluster[vm_num]
+    same_cluster = []
+    # 寻找同集群的vm（自身除外）
+    for i in range(d.vm_count):
+        if d.vm_mycluster[i] == cluster_num and i != vm_num:
+            same_cluster.append(i)
+    # 如果同集群为空（无同集群vm），则返回0（说明无需vm间通信）
+    if same_cluster == []:
+        return 0
+    # 对同集群除自身外所有vm，计算跳数并累加
+    total_hop = 0
+    for i in same_cluster:
+        hop = d.hop_matrix[(omiga[vm_num], d.vm_mypm[i])]
+        total_hop += hop
+    return total_hop / 2
+
+
+def cal_Rmi(Umi):
+    return 1+math.log(1-(1-1/math.e)*Umi)
+
+
 # Read input file
-data = Data(sys.argv[1])
-# nodes = range(data.size)
+d = Data(sys.argv[1])
 model = Model("mv-migrate")
 # model.setParam(GRB.Param.TimeLimit, 60 * 60 * 10.0) # 10 hour
 
 # Variables
-rho = []  # ρ(i) i个vm
+rho = {}  # ρ(i) i个vm是否迁移
+omiga = {}  # ω(i) vm迁移到目标pm的位置
 
-for i in range(data.vm_count):
+for i in range(d.vm_count):
     rho[i] = model.addVar(vtype=GRB.BINARY, name=f"rho_{i}")
+
+for i in range(d.vm_count):
+    omiga[i] = model.addVar(
+        vtype=GRB.INTEGER, name=f"omiga_{i}", lb=1, ub=d.pm_count)
 
 # Update model
 model.update()
 
 # Add constraints
-# TODO
+# 大M法
+M = 10000000
+b = [model.addVar(vtype=GRB.BINARY, name=f"b_{i}") for i in range(d.vm_count)]
+for i in range(d.vm_count):
+    model.addConstr((rho[i] == 0) >> (omiga[i] == d.vm_mypm[i]))
+    # model.addConstr((rho[i] == 1) >> (omiga[i] != d.vm_mypm[i]))
+    model.addConstr((rho[i] == 1) >> (
+        omiga[i] <= d.vm_mypm[i] - 1 + M * (1 - b[i])))
+    # 当 b[i] = 1 时，强制 omega[i] <= d.vm_mypm[i] - 1
+    model.addConstr((rho[i] == 1) >> (omiga[i] >= d.vm_mypm[i] + 1 - M * b[i]))
+    # 当 b[i] = 0 时，强制 omega[i] >= d.vm_mypm[i] + 1
+
+for i in range(1, d.pm_count + 1):
+    pm_core_sum = quicksum(d.vm_CPUcore[j] for j in range(d.vm_count) if omiga[j] == i)
+    Uc = quicksum(d.vm_CPUuti[j] * d.vm_CPUcore[j] for j in range(d.vm_count) if omiga[j] == i) / pm_core_sum
+    Uo = pm_core_sum / d.pm_CPUcore[i-1]
+    Ur = quicksum(d.vm_storage[j] for j in range(d.vm_count) if omiga[j] == i) / d.pm_storage[i-1]
+    Ub = quicksum(d.vm_bw[j] for j in range(d.vm_count) if omiga[j] == i) / d.pm_bw[i-1]
+    model.addConstr(cal_Rmi(Uc) >= d.elasticity_lb)
+    model.addConstr(cal_Rmi(Uo) >= d.elasticity_lb)
+    model.addConstr(cal_Rmi(Ur) >= d.elasticity_lb)
+    model.addConstr(cal_Rmi(Ub) >= d.elasticity_lb)
+    # model.addConstr(pm_core_sum <= d.pm_CPUcore[i-1])
+    model.addConstr(Uc <= 1)
+    model.addConstr(Uo <= 1)
+    model.addConstr(Ur <= 1)
+    model.addConstr(Ub <= 1)
+    model.addConstr(Uc >= 0)
+    model.addConstr(Uo >= 0)
+    model.addConstr(Ur >= 0)
+    model.addConstr(Ub >= 0)
 
 # Objective function
-model.setObjective(quicksum(data.vm_bandwidth[i] * data.band_unitcost + data.vm_storage[i] * rho[i] for i in range(data.vm_count)))
+model.setObjective(quicksum(d.cluster_innerpakage_size[d.vm_mycluster[i]] * cal_hops(
+    i) * d.band_unitcost + d.vm_storage[i] * rho[i] for i in range(d.vm_count)))
 
 # Save LP model and run
 model.write('model.rlp')
@@ -40,10 +120,13 @@ if model.status != GRB.OPTIMAL:
     print('No optimum solution found. Status: %i' % (model.status))
 else:
     print("Optimal solution found")
-    rho_result = [i for i in range(data.vm_count) if rho[i].X > 0.9]
+    rho_result = [1 if rho[i].X > 0.9 else 0 for i in range(d.vm_count)]
+    omiga_result = [omiga[i].X for i in range(d.vm_count)]
     print("Result = %.4f" % model.objVal)
     print("GAP = %.4f %%" % model.MIPGap)
     print("Time = %.4f seg" % model.Runtime)
 
     print("ρ(i): ")
     print(rho_result)
+    print("ω(i): ")
+    print(omiga_result)
